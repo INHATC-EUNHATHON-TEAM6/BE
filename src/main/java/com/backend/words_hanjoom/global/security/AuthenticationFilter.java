a866c6724091325bd6966cbee7380ac129c21ad5
YejiJeonn <yejijeon02@gmail.com>
2025-08-06 12:09:21 +0900

    :sparkles: feat: add file structure

---
+++ src/main/java/com/backend/words_hanjoom/global/security/AuthenticationFilter.java
@@ -0,0 +1,51 @@
+package com.backend.words_hanjoom.global.security;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * 토큰 인증 필터
+ *
+ * 헤더에서 토큰을 가져와서 유효한 토큰인지 확인하고,
+ * 유효하다면 SecurityContextHolder에 Authentication 객체를 저장하고 다음 필터로 넘어가도록 함
+ */
+@Log4j2
+@RequiredArgsConstructor
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        // AUTHORIZATION 헤더에서 Bearer Token 추출
+        String accessToken = HeaderUtil.getAccessTokenFromHeader(request);
+
+        Claims claims = tokenProvider.getClaims(accessToken);
+
+        // 토큰이 유효하면 클레임을 가져옴
+        if (claims != null) {
+            // claims 이용하여 Authentication 객체를 생성
+            Authentication authentication = tokenProvider.getAuthentication(claims, accessToken);
+
+            // SecurityContextHolder에 Authentication 객체를 저장
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
