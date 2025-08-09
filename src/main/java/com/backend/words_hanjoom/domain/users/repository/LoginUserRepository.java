a99b52ab6976c3f9a6007658170c996bd6441df9
YejiJeonn <yejijeon02@gmail.com>
2025-08-09 22:11:43 +0900

    수정

--- src/main/java/com/backend/words_hanjoom/domain/users/repository/LoginUserRepository.java
+++ src/main/java/com/backend/words_hanjoom/domain/users/repository/LoginUserRepository.java
@@ -1,11 +1,10 @@
 package com.backend.words_hanjoom.domain.users.repository;
 
 import com.backend.words_hanjoom.domain.users.entity.LoginUserEntity;
-import com.hdd.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface LoginUserRepository extends JpaRepository<User, Long> {
+public interface LoginUserRepository extends JpaRepository<LoginUserEntity, Long> {
      Optional<LoginUserEntity> findUserByLoginId(String loginId);
 }
