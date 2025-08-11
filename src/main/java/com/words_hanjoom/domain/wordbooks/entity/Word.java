6c54d60b842e5577c9686174da7b6cfc457270e4
zzingni <wldms001313@gamil.com>
2025-08-12 01:09:25 +0900

    ✨ feat: create Word entity

---
+++ src/main/java/com/words_hanjoom/domain/wordbooks/entity/Word.java
@@ -0,0 +1,50 @@
+package com.words_hanjoom.domain.wordbooks.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "words")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Word {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "word_id")
+    private Integer wordId;
+
+    @Column(name = "word_name", length = 100, nullable = false)
+    private String wordName;
+
+    @Column(name = "synonym", length = 100, nullable = false)
+    private String synonym;
+
+    @Column(name = "antonym", length = 100, nullable = false)
+    private String antonym;
+
+    @Column(name = "definition", columnDefinition = "TEXT", nullable = false)
+    private String definition;
+
+    @Column(name = "word_category", length = 30, nullable = false)
+    private String wordCategory;
+
+    @Column(name = "shoulder_no", nullable = false)
+    private Byte shoulderNo;
+
+    @Column(name = "example", columnDefinition = "TEXT", nullable = false)
+    private String example;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
