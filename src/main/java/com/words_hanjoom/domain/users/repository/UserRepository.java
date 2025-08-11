40169462533f0754bf2bece12255abcb441db6cb
zzingni <wldms001313@gamil.com>
2025-08-11 21:11:34 +0900

    ✨ feat: add UserRepository, CategoryRepository, and UserCategoryRepository

--- src/main/java/com/words_hanjoom/domain/users/repository/UserRepository.java
+++ src/main/java/com/words_hanjoom/domain/users/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.words_hanjoom.domain.users.repository;
 import com.words_hanjoom.domain.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
