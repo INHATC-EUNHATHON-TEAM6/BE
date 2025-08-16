bdec7ca01daffe9a4b052f67c6b0bd68e657bc54
jhdgo1225 <parkjh99365@gmail.com>
2025-08-17 01:22:21 +0900

    FIX: 환경변수 등록이 안 되는 문제 해결
    main 메서드(WordsHanjoomApplication.java 파일)에 dotenv 활용을 안 해서 환경변수를 인식 못하는 이슈가 있었습니다.
    dotenv 적용 후 해결 됐습니다.

--- src/main/java/com/words_hanjoom/WordsHanjoomApplication.java
+++ src/main/java/com/words_hanjoom/WordsHanjoomApplication.java
@@ -1,5 +1,6 @@
 package com.words_hanjoom;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WordsHanjoomApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")  // .env 파일 위치 (프로젝트 루트)
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        // 환경 변수로 시스템 프로퍼티 설정
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(WordsHanjoomApplication.class, args);
     }
 
