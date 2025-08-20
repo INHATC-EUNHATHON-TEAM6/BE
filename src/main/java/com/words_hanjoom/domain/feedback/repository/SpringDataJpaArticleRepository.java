40abf73261ce7a63bf92fc04c32c8d7f0d370bd3
jhdgo1225 <parkjh99365@gmail.com>
2025-08-20 23:08:40 +0900

    FEAT: 특정 스크랩 활동 ㅍ피드백 내용 조회 API 레포지토리 구현

--- src/main/java/com/words_hanjoom/domain/feedback/repository/SpringDataJpaArticleRepository.java
+++ src/main/java/com/words_hanjoom/domain/feedback/repository/SpringDataJpaArticleRepository.java
@@ -2,6 +2,8 @@ package com.words_hanjoom.domain.feedback.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.words_hanjoom.domain.feedback.entity.Article;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpringDataJpaArticleRepository extends JpaRepository<Article, Long> {
 }
