cc5044bd9422fdeb7ac6a44df4d0f537c3b0ffc5
jhdgo1225 <parkjh99365@gmail.com>
2025-08-22 18:47:39 +0900

    CHORE: OpenAPI, OpenAI API 모델 설정, JWT 시크릿 키 및 JWT 기한 프로퍼티 설정

--- src/main/java/com/words_hanjoom/domain/feedback/repository/SpringDataJpaCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.words_hanjoom.domain.feedback.repository;
-
-import com.words_hanjoom.domain.feedback.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpringDataJpaCategoryRepository  extends JpaRepository<Category, Long> {
-}
